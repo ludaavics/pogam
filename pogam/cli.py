9c4d177bb865cf70ac5966f63a9dc350e4d68185
Ludovic Tiako <ludovic.tiako@gmail.com>
2019-11-24 13:12:30 -0500

    Add schedule command

--- pogam/cli.py
+++ pogam/cli.py
@@ -1,6 +1,9 @@
 import logging
+import json
+import os
 from typing import Iterable, List
 
+import boto3
 import click
 import click_log  # type: ignore
 
@@ -14,6 +17,7 @@ app = create_app("cli")
 TRANSACTION_TYPES = ["rent", "buy"]
 PROPERTY_TYPES = ["apartment", "house", "parking", "store"]
 SOURCES = ["seloger"]
+S3_TASKS_FILENAME = "tasks.json"
 
 
 @click.group()
@@ -23,7 +27,7 @@ def cli():
     pass
 
 
-@click.command()
+@cli.command(name="scrape")
 @click.argument("transaction")
 @click.argument("post_codes", nargs=-1)
 @click.option(
@@ -128,4 +132,153 @@ def scrape_cmd(
     click.echo(msg)
 
 
-cli.add_command(scrape_cmd, name="scrape")
+@cli.group()
+def schedule():
+    """Schedule scrapes to run automatically."""
+
+
+@schedule.command(name="add")
+@click.argument("transaction")
+@click.argument("post_codes", nargs=-1)
+@click.option(
+    "--type",
+    "property_types",
+    multiple=True,
+    type=click.Choice(PROPERTY_TYPES, case_sensitive=False),
+    default=["apartment", "house"],
+    help="Type of property.",
+)
+@click.option("--min-price", type=float, help="Minimum property price.")
+@click.option("--max-price", type=float, help="Maximum property price.")
+@click.option("--min-size", type=float, help="Minimum property size, in square meters.")
+@click.option("--max-size", type=float, help="Maximum property size, in square meters.")
+@click.option("--min-rooms", type=float, help="Minimum number of rooms.")
+@click.option("--max-rooms", type=float, help="Maximum number of rooms.")
+@click.option("--min-beds", type=float, help="Minimum number of bedrooms.")
+@click.option("--max-beds", type=float, help="Maximum number of bedrooms.")
+@click.option(
+    "--num-results",
+    type=int,
+    default=100,
+    show_default=True,
+    help="Approximate maximum number of listings to add to the database.",
+)
+@click.option(
+    "--max-duplicates",
+    type=int,
+    default=25,
+    help=(
+        "Stop further scrapes once we see this many consecutive results that are "
+        "already in the database."
+    ),
+)
+@click.option(
+    "--sources",
+    multiple=True,
+    type=click.Choice(SOURCES, case_sensitive=False),
+    help="Sources to scrape.",
+)
+def schedule_add(
+    transaction: str,
+    post_codes: Iterable[str],
+    property_types: Iterable[str],
+    min_price: float,
+    max_price: float,
+    min_size: float,
+    max_size: float,
+    min_rooms: float,
+    max_rooms: float,
+    min_beds: float,
+    max_beds: float,
+    num_results: int,
+    max_duplicates: int,
+    sources: Iterable[str],
+):
+    """
+    Schedules the scrape of offers for a TRANSACTION in the given POST_CODES.
+
+    TRANSACTION is 'rent' or 'buy'.
+    POSTCODES are postal or zip codes of the search.
+    """
+    if transaction.lower() not in TRANSACTION_TYPES:
+        raise ValueError(f"Unexpected transaction type {transaction}.")
+    if not sources:
+        sources = SOURCES
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(_bucket_name())
+    try:
+        tasks = json.loads(bucket.Object(S3_TASKS_FILENAME).get()["Body"].read())
+    except s3.meta.client.exceptions.NoSuchKey:
+        tasks = []
+
+    search = {
+        "transaction": transaction,
+        "post_codes": post_codes,
+        "property_types": property_types,
+        "min_price": min_price,
+        "max_price": max_price,
+        "min_size": min_size,
+        "max_size": max_size,
+        "min_rooms": min_rooms,
+        "max_rooms": max_rooms,
+        "min_beds": min_beds,
+        "max_beds": max_beds,
+        "num_results": num_results,
+        "max_duplicates": max_duplicates,
+        "sources": sources,
+    }
+    search = json.loads(json.dumps(search))
+    if search in tasks:
+        msg = "This search is already scheduled!"
+        click.echo(msg)
+        return
+
+    tasks.append(search)
+    bucket.put_object(
+        ContentType="application/json",
+        Key=S3_TASKS_FILENAME,
+        Body=json.dumps(schedule, indent=2),
+    )
+    msg = (
+        f"{color.BOLD}All done!✨ 🍰 ✨{color.END}\n"
+        f"We now have {len(tasks)} search{'es' if len(tasks) > 1 else ''} "
+        f"scheduled."
+    )
+    click.echo(msg)
+    return
+
+
+@schedule.command(name="list")
+def schedule_list():
+    """List all scheduled tasks in JSON format."""
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(_bucket_name())
+    try:
+        tasks = json.loads(bucket.Object(S3_TASKS_FILENAME).get()["Body"].read())
+    except s3.meta.client.exceptions.NoSuchKey:
+        tasks = []
+    click.echo(json.dumps(tasks, indent=2))
+
+
+@schedule.command(name="clear")
+def schedule_clear():
+    """Clear all scheduled tasks."""
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(_bucket_name())
+    try:
+        tasks = bucket.Object(S3_TASKS_FILENAME)
+    except s3.meta.client.exceptions.NoSuchKey:
+        pass
+
+    tasks.delete()
+
+    msg = "All scheduled tasks have been deleted."
+    click.echo(msg)
+
+
+def _bucket_name():
+    try:
+        return os.environ["POGAM_AWS_BUCKET_NAME"]
+    except KeyError:
+        msg = f"Missing environment variable 'POGAM_AWS_BUCKET_NAME'."
+        raise RuntimeError(msg)
