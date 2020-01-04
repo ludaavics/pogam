a75afbb9f8c5a1bc9310403891dfbfb52077cc03
Ludovic Tiako <ludovic.tiako@gmail.com>
2020-01-04 14:35:54 -0500

    Add initial integraton test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,94 @@
 import os
-
+import subprocess
+import uuid
+import logging
 import pytest
 
+here = os.path.dirname(__file__)
+root_folder = os.path.abspath(os.path.join(here, ".."))
+logger = logging.getLogger(__name__)
 
+POGAM_API_HOST = "https://api.pogam-estate.local/"  # could be anything
+
+
+# ------------------------------------------------------------------------------------ #
+#                          Command Line Options Configuration                          #
+# ------------------------------------------------------------------------------------ #
+def pytest_addoption(parser):
+    parser.addoption(
+        "--deploy-aws",
+        action="store_true",
+        default=False,
+        help="Run tests that require the deployment of AWS services.",
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "aws: mark test as requiring the deployment of AWS services"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--deploy-aws"):
+        return
+    skip_aws = pytest.mark.skip(
+        reason="Requires deployed AWS services. Use --deploy-aws option to run."
+    )
+    for item in items:
+        if "aws" in item.keywords:
+            item.add_marker(skip_aws)
+
+
+# ------------------------------------------------------------------------------------ #
+#                                        Fixtures                                      #
+# ------------------------------------------------------------------------------------ #
 @pytest.fixture
 def api_host():
     original_host = os.getenv("POGAM_API_HOST")
-    test_host = os.getenv("POGAM_TEST_API_HOST")
+    test_host = POGAM_API_HOST
     if test_host is None:
         msg = "Missing 'POGAM_API_HOST' environment variable."
         raise ValueError(msg)
     os.environ["POGAM_API_HOST"] = test_host
-    yield
+    yield test_host
     os.environ["POGAM_API_HOST"] = original_host
+
+
+# ----------------------------- AWS Services Fixtures -------------------------------- #
+@pytest.fixture(scope="session")
+def stage():
+    return f"test-{str(uuid.uuid4()).split('-')[0]}"
+
+
+@pytest.fixture
+def s3_resources_service(stage):
+    logger.info(f"Deploying s3-resources service to stage {stage}...")
+    folder = os.path.join(root_folder, "app", "s3-resources")
+    subprocess.run(["sls", "remove", "--stage", stage], cwd=folder)
+    subprocess.run(["sls", "deploy", "--stage", stage], cwd=folder)
+    yield
+    logger.info(f"Taking down s3-resources service from stage {stage}...")
+    subprocess.run(["sls", "remove", "--stage", stage], cwd=folder)
+
+
+@pytest.fixture
+def scrapes_api_service(s3_resources_service, stage):
+    logger.info(f"Deploying scrapes-api service to stage {stage}...")
+    folder = os.path.join(root_folder, "app", "scrapes-api")
+    subprocess.run(["sls", "remove", "--stage", stage], cwd=folder)
+    subprocess.run(["sls", "deploy", "--stage", stage], cwd=folder)
+    yield
+    logger.info(f"Taking down scrapes-api service from stage {stage}...")
+    subprocess.run(["sls", "remove", "--stage", stage], cwd=folder)
+
+
+@pytest.fixture
+def scrape_schedules_api_service(scrapes_api_service, stage):
+    logger.info(f"Deploying scrape-schedule-api service to stage {stage}...")
+    folder = os.path.join(root_folder, "app", "scrape-schedules-api")
+    subprocess.run(["sls", "remove", "--stage", stage], cwd=folder)
+    subprocess.run(["sls", "deploy", "--stage", stage], cwd=folder)
+    yield
+    logger.info(f"Taking down scrape-schedule-api service from stage {stage}...")
+    subprocess.run(["sls", "remove", "--stage", stage], cwd=folder)
--- tests/test_scrape_schedules.py
+++ tests/test_scrape_schedules.py
@@ -1,41 +1,82 @@
-from pogam.cli import cli
-from click.testing import CliRunner
-import pytest
-from httmock import urlmatch, response, HTTMock
-from requests.compat import urlparse
+import json
 import os
+import subprocess
+
+import pytest
+from click.testing import CliRunner
+from httmock import HTTMock, response, urlmatch
+from requests.compat import urlparse
+
+from pogam.cli import cli
 
 here = os.path.dirname(__file__)
-
-
-POGAM_API_HOST = "https://api.pogam-estate.local/"  # could be anything
-netloc = urlparse(POGAM_API_HOST).netloc
+root_folder = os.path.abspath(os.path.join(here, ".."))
+service_folder = os.path.join(root_folder, "app", "scrape-schedules-api")
 
 
 # ------------------------------------------------------------------------------------ #
 #                                       Fixtures                                       #
 # ------------------------------------------------------------------------------------ #
 @pytest.fixture
-def endpoint_list():
+def list_response(api_host):
     """Return a mock API response for a scrape schedules listing request."""
-    f = os.path.join(here, "fixtures/scrape-schedules-api/list.json")
+    f = os.path.join(here, "fixtures/scrape-schedules-api/list-response.json")
     with open(f) as f:
-        resp = f.read()
+        resp = f.read()[:-1]
+    netloc = urlparse(api_host).netloc
 
-    @urlmatch(netloc=netloc, path="/v1/scrape-schedules/", method="get")
+    @urlmatch(netloc=netloc, path="/v1/scrape-schedules", method="get")
     def mock_response(url, request):
         return response(200, resp)
 
     return {"mock_response": mock_response, "expected_response": resp}
 
 
+@pytest.fixture
+def list_event():
+    """Return an API Gateway event for the list handler."""
+    f = os.path.join(here, "fixtures/scrape-schedules-api/list-event.json")
+    return str(f)
+    with open(f) as f:
+        event = f.read()
+    return event
+
+
 # ------------------------------------------------------------------------------------ #
 #                                         Tests                                        #
 # ------------------------------------------------------------------------------------ #
 class TestCli(object):
-    def test_app_scrape_schedules_list(self, api_host, endpoint_list):
+    def test_list(self, api_host, list_response):
+        """CLI command for listing scheduled scrapes."""
         runner = CliRunner()
-        with HTTMock(endpoint_list["mock_response"]):
+        with HTTMock(list_response["mock_response"]):
             result = runner.invoke(cli, ["app", "scrape-schedules", "list"])
         assert result.exit_code == 0
-        assert endpoint_list["expected_response"] == result.output
+        expected = json.loads(list_response["expected_response"])
+        actual = json.loads(result.output)
+        assert expected == actual
+
+
+class TestHandlers(object):
+    @pytest.mark.aws
+    def test_crud(self, stage, scrape_schedules_api_service, list_event, snapshot):
+        """Scrape Schedules CRUD handlers"""
+        # list (empty) schedules
+        result = subprocess.run(
+            [
+                "sls",
+                "invoke",
+                "--stage",
+                stage,
+                "--function",
+                "list",
+                "--path",
+                list_event,
+            ],
+            cwd=service_folder,
+            capture_output=True,
+        )
+
+        assert result.returncode == 0
+        actual = json.loads(result.stdout)
+        snapshot.assert_match(actual)
