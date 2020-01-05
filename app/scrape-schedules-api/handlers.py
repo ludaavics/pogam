fea17eb4d90d304b139c3eab0e38f942312ab28f
Ludovic Tiako <ludovic.tiako@gmail.com>
2020-01-05 13:39:17 -0500

    Remove print statements

--- app/scrape-schedules-api/handlers.py
+++ app/scrape-schedules-api/handlers.py
@@ -17,7 +17,6 @@ def create(event, context):
     """
     Create a rule to scrape a given search on a given schedule.
     """
-    print(event)
     stage = os.environ["STAGE"]
     data = json.loads(event.get("body", "{}"))
     if "schedule" not in data or "search" not in data:
@@ -112,7 +111,6 @@ def list_(event, context):
     """
     List scheduled scrapes.
     """
-    print(event)
     cloudwatch_events = boto3.client("events")
     rule_name = f"pogam-{os.environ['STAGE']}-"
     rules = cloudwatch_events.list_rules(NamePrefix=rule_name)["Rules"]
@@ -139,7 +137,6 @@ def delete(event, context):
     """
     Delete a given scheduled scrape.
     """
-    print(event)
     cloudwatch_events = boto3.client("events")
     rule_name = event["pathParameters"]["rule_name"]
     rules = cloudwatch_events.list_rules(NamePrefix=rule_name)["Rules"]
