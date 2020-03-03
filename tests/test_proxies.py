88a0a973c1b2c32373348b507f5d4c4bb3f29920
Ludovic Tiako <ludovic.tiako@gmail.com>
2020-03-02 20:15:53 -0500

    Mock proxy APIs

--- tests/test_proxies.py
+++ tests/test_proxies.py
@@ -5,7 +5,7 @@ import re
 import warnings
 
 import pytest
-from httmock import HTTMock, all_requests
+from httmock import HTTMock, all_requests, response, urlmatch
 
 from pogam.scrapers import proxies
 
@@ -24,10 +24,70 @@ is_ip_address = re.compile(
 @pytest.fixture
 def unavailable_proxy():
     @all_requests
-    def response(url, request):
+    def mock_response(url, request):
         return {"status_code": 500, "content": ""}
 
-    return response
+    return mock_response
+
+
+@pytest.fixture
+def mock_proxylist():
+    content = """
+    71.123.91.130
+    32.128.172.100
+    246.35.254.239
+    68.222.8.108
+    101.250.230.30
+    140.35.255.230
+    177.3.77.227
+    159.244.36.216
+    98.220.71.235
+    28.162.155.253
+    237.45.242.157
+    132.88.231.167
+    179.236.65.189
+    197.97.138.184
+    151.255.68.118
+    """
+
+    @urlmatch(netloc="www.proxy-list.download")
+    def mock_response(url, request):
+        return response(200, content=content, headers={"Content-Type": "text/plain"})
+
+    return mock_response
+
+
+@pytest.fixture
+def mock_proxy11():
+    content = """
+    1.0.0.103:80
+    1.0.0.5:80
+    1.0.0.92:80
+    87.255.27.169:3128
+    51.158.108.135:8811
+    104.17.116.167:80
+    1.0.0.45:80
+    178.62.204.137:80
+    1.0.0.253:80
+    188.166.83.17:3128
+    84.17.47.182:80
+    213.175.42.220:3128
+    1.0.0.34:80
+    1.0.0.141:80
+    95.31.119.210:31135
+    176.114.128.131:3128
+    """
+
+    @urlmatch(netloc="proxy11.com")
+    def mock_response(url, request):
+        return response(200, content=content, headers={"Content-Type": "text/plain"})
+
+    return mock_response
+
+
+@pytest.fixture
+def mock_proxies(mock_proxylist, mock_proxy11):
+    return [mock_proxylist, mock_proxy11]
 
 
 @pytest.fixture(
@@ -61,7 +121,7 @@ def no_proxy11_api_key():
 @pytest.mark.parametrize("errors", ["raise", "warn"])
 @pytest.mark.parametrize("proxy_is_down", [True, False])
 def test_get_proxy_pool(
-    infinite, errors, proxy_calls, proxy_is_down, unavailable_proxy
+    infinite, errors, proxy_calls, proxy_is_down, unavailable_proxy, mock_proxies
 ):
     """
     Create a proxy pool from a given source.
@@ -72,7 +132,7 @@ def test_get_proxy_pool(
         proxy_kwargs.update({"api_key": os.getenv("PROXY11_API_KEY")})
 
     if proxy_is_down:
-        request_context_manager = HTTMock(unavailable_proxy)
+        request_context_managers = [unavailable_proxy]
 
         if errors == "warn":
             match = re.compile(r"^Failed to get.*Proceeding.*", re.DOTALL)
@@ -82,13 +142,15 @@ def test_get_proxy_pool(
             match = r"(?:^Failed\S*)((?!Proceeding).)*$"
             pytest_context_manager = pytest.raises(RuntimeError, match=match)
     else:
-        request_context_manager = contextlib.nullcontext()
+        request_context_managers = mock_proxies
         pytest_context_manager = contextlib.nullcontext()
 
     warning_filter = (
         "ignore" if (proxy_name == "all_proxies" and errors == "raise") else "default"
     )
-    with request_context_manager:
+    with contextlib.ExitStack() as stack:
+        for request_context_manager in request_context_managers:
+            stack.enter_context(HTTMock(request_context_manager))
         with warnings.catch_warnings():
             warnings.simplefilter(warning_filter)
             with pytest_context_manager:
