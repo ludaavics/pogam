8d4bc0138ed157a96bd009390fabaf0a1876e93e
Ludovic Tiako <ludovic.tiako@gmail.com>
2020-03-05 21:39:33 -0500

    Search accross multiple pages

--- tests/test_leboncoin.py
+++ tests/test_leboncoin.py
@@ -1,14 +1,14 @@
 import json
 import os
+from copy import deepcopy
+import math
 
 import pytest
-from httmock import HTTMock, response, urlmatch
 import requests
+from httmock import HTTMock, response, urlmatch
 
 from pogam import create_app
 from pogam.scrapers.leboncoin import leboncoin
-
-app = create_app("cli")
 
 here = os.path.dirname(__file__)
 root_folder = os.path.abspath(os.path.join(here, ".."))
@@ -25,11 +25,23 @@ def make_search_and_response():
             search_and_response = json.load(f)
         search = search_and_response["search"]
         _response = search_and_response["response"]
+        original_ads = deepcopy(_response["ads"])
+
+        page = 0
+        page_length = 5
+        last_page = math.ceil(len(original_ads) / page_length)
 
         @urlmatch(
             netloc="api.leboncoin.fr", path="/api/adfinder/v1/search", method="post"
         )
         def mock_response(url, request):
+            nonlocal page
+            _response["ads"] = original_ads[
+                page * page_length : (page + 1) * page_length
+            ]
+            if page == last_page:
+                _response.pop("pivot")
+            page += 1
             return response(200, _response, request=request)
 
         return {"search": search, "response": mock_response}
@@ -68,6 +80,7 @@ def test_known_query(name, make_search_and_response, mock_proxies, in_memory_db)
     search_and_response = make_search_and_response(name)
     search = search_and_response["search"]
     mock_response = search_and_response["response"]
+    app = create_app("cli")
     with HTTMock(mock_response):
         with app.app_context():
             leboncoin(**search)
@@ -80,6 +93,7 @@ def test_request_error(exception_name, make_error_response, mock_proxies, in_mem
         "timeout": requests.exceptions.Timeout,
     }[exception_name]
     mock_response = make_error_response(exception)
+    app = create_app("cli")
     with HTTMock(mock_response):
         with app.app_context():
             match = r"Failed to reach .*"
@@ -88,6 +102,7 @@ def test_request_error(exception_name, make_error_response, mock_proxies, in_mem
 
 
 def test_captcha(mock_captcha, mock_proxies, in_memory_db):
+    app = create_app("cli")
     with HTTMock(mock_captcha):
         with app.app_context():
             match = r"Failed to reach .*"
